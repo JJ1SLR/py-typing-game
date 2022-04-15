364f8b3f3e703c2cfd20568721a015270d3d19d4
JJ1SLR <JJ1SLR@jarl.com>
2022-04-15 21:42:43 +0900

    Hello World.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import sys
+import string
+import random
+
 import pygame
 
 
@@ -9,11 +12,13 @@ class MainWindow:
         self.height = height
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
-        self.speed = [2, 2]
+        self.speed = [3, 1]
+        self.letter = random.choice(string.ascii_uppercase)
+        self.letter_change_counter = 0
 
     def main_loop(self):
         font = pygame.font.Font(None, 55)
-        text = font.render("Hello, world!", True, (255,255,255))
+        text = font.render(self.letter, True, (255, 255, 255))
         text_rect = text.get_rect()
 
         while True:
@@ -21,7 +26,7 @@ class MainWindow:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.clock.tick(60)
+            self.clock.tick(50)
 
             text_rect = text_rect.move(self.speed)
             if text_rect.left < 0 or text_rect.right > self.width:
@@ -29,7 +34,13 @@ class MainWindow:
             if text_rect.top < 0 or text_rect.bottom > self.height:
                 self.speed[1] = -self.speed[1]
 
-            self.screen.fill((0,0,0))
+            self.letter_change_counter += 1
+            if self.letter_change_counter % 30 == 0:
+                self.letter = random.choice(string.ascii_uppercase)
+                text = font.render(self.letter, True, (255, 255, 255))
+                self.letter_change_counter = 0
+
+            self.screen.fill((0, 0, 0))
             self.screen.blit(text, text_rect)
             pygame.display.flip()
 
