32f77fe8ee89f2779af0772b04baa72e7b725cd4
JJ1SLR <JJ1SLR@jarl.com>
2022-04-21 23:08:44 +0900

    Move scene_type to parameter.

--- src/main.py
+++ src/main.py
@@ -9,12 +9,12 @@ ONE_LETTER_SCENE = 1
 
 class MainWindow:
 
-    def __init__(self, width: int = 640, height: int = 480):
+    def __init__(self, width: int = 640, height: int = 480, scene_type: int = ONE_LETTER_SCENE):
         pg.init()
         self.screen = pg.display.set_mode((width, height))
         pg.display.set_caption("Typing Game")
         self.clock = pg.time.Clock()
-        self.scene = self.get_scene(ONE_LETTER_SCENE)
+        self.scene = self.get_scene(scene_type)
 
     def main_loop(self):
         while True:
