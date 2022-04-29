e969ab4298c6f034a8e0741bcf9d1e8fbcc264bb
JJ1SLR <JJ1SLR@jarl.com>
2022-04-29 13:26:19 +0900

    Add reset button to one letter scene.

--- src/one_letter_scene.py
+++ src/one_letter_scene.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable
 if TYPE_CHECKING:
     from main import MainWindow
 
@@ -9,6 +9,7 @@ from src.pgf.letter import Letter
 from src.pgf.scene import Scene
 from score_board import ScoreBoard
 
+from src.pgf.button import Button
 
 class OneLetterScene(Scene):
 
@@ -17,10 +18,20 @@ class OneLetterScene(Scene):
         self.letter = Letter(0, 0, parent=self.root_widget)
         self.letter.set_center()
         self.scoreBoard = ScoreBoard(0, 0, parent=self.root_widget)
+        self.reset_button = Button(self.get_width() - 165, 5, 160, 120, 50, self.root_widget, "Reset")
+        self.reset_button.set_mouse_up_handler(self.create_on_button_reset())
         self.clickSnd = pg.mixer.Sound("../sound/click.wav")
         self.errorSnd = pg.mixer.Sound("../sound/error.wav")
         self.comboSnd = pg.mixer.Sound("../sound/combo.wav")
         self.correct = False
+
+    def create_on_button_reset(self) -> Callable:
+        def on_button_reset(_button: Button, _event: pg.event.Event, _handled: bool) -> bool:
+            self.scoreBoard.reset()
+            self.letter.next()
+            self.letter.set_center()
+            return True
+        return on_button_reset
 
     def on_key_down(self, event: pg.event.Event):
         if pg.K_a <= event.key <= pg.K_z:
